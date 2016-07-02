13a5db6e1ba1b768e1d455c6905b0b5323342de0
Mikaela Patella <m.c.patella@gmail.com>
2016-07-01 17:32:31 -0700

    clean out boilerplate comments in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject web-dev-dist-sys "0.1.0-SNAPSHOT"
   :description "Slides application for Web Dev is Dist Sys."
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
@@ -17,8 +16,7 @@
                  [ring/ring-defaults "0.2.0"]
                  [compojure "1.5.0"]
                  [hiccup "1.0.5"]
-                 [reagent "0.5.1"]
-                 ]
+                 [reagent "0.5.1"]]
 
   :plugins [[lein-figwheel "0.5.2"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
@@ -38,7 +36,6 @@
               [{:id "dev"
                 :source-paths ["src"]
 
-                ;; If no code is to be run, set :figwheel true for continued automagical reloading
                 :figwheel {:on-jsload "web-dev-dist-sys.client/on-js-reload"}
 
                 :compiler {:main web-dev-dist-sys.client
@@ -46,9 +43,6 @@
                            :output-to "resources/public/js/compiled/web_dev_dist_sys.js"
                            :output-dir "resources/public/js/compiled/out"
                            :source-map-timestamp true}}
-               ;; This next build is an compressed minified build for
-               ;; production. You can build this with:
-               ;; lein cljsbuild once min
                {:id "min"
                 :source-paths ["src"]
                 :compiler {:output-to "resources/public/js/compiled/web_dev_dist_sys.js"
@@ -56,33 +50,5 @@
                            :optimizations :advanced
                            :pretty-print false}}]}
 
-  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
-
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+  :figwheel {:css-dirs ["resources/public/css"] ;; watch and update CSS 
              })
